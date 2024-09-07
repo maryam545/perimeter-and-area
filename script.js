3d104ab6b435b66246ba8529a6db0345c7ecd25d
maryam <marychch19@gmail.com>
2024-09-07 13:23:10 +0330

    rectanglePerimeter

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let width = 6;
 let height = 10;
 const rectangleArea = width * height;
-console.log(rectangleArea);
+console.log(`rectangleArea = ` + rectangleArea);
+const rectanglePerimeter = width * 2 + height * 2;
+console.log(`rectanglePerimeter = ` + rectanglePerimeter);
