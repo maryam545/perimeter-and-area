66a8064812f20ff1de68d63e42f4f3dbbfbfc6fa
maryam <marychch19@gmail.com>
2024-09-07 13:59:13 +0330

    circle trapezoid

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+// start rectangle
 let width = 6;
 let height = 10;
 const rectangleArea = width * height;
 console.log(`rectangleArea = ` + rectangleArea);
 const rectanglePerimeter = width * 2 + height * 2;
 console.log(`rectanglePerimeter = ` + rectanglePerimeter);
+// end rectangle
+// start circle
+let pi = 3.14;
+let radius = 2;
+const circleArea = pi * radius * 2;
+console.log(`circleArea = ` + circleArea);
+const circlePerimeter = radius * radius * pi;
+console.log(`circlePerimeter = ` + circlePerimeter);
+
+// end circle
+// start trapezoid
+let smallBase = 2;
+let bigBase = 4;
+let heightTrapezoid = 6;
+const trapezoidArea = 2 * smallBase + 2 * bigBase;
+console.log(`trapezoidArea = ` + trapezoidArea);
+const trapezoidPerimeter = ((smallBase + bigBase) * heightTrapezoid) / 2;
+console.log(`trapezoidPerimeter = ` + trapezoidPerimeter);
+// end trapezoid
